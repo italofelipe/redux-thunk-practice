fedd4073c4ac0a3c2bcd2330f36a283d8a2fd605
Italo Felipe <felipe.italo@hotmail.com>
2019-06-28 14:28:38 -0300

    Aula 186: Resolvido o problema de Overfetching graças ao memoization do Lodash, implementado nas Actions.

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,11 +1,16 @@
 import API from "../utils/API";
+import _ from "lodash";
 export const fetchPosts = () => async dispatch => {
   const response = await API.get("/posts");
 
   dispatch({ type: "FETCH_POSTS", payload: response.data });
 };
-export const fetchUser = id => async dispatch => {
+export const fetchUser = id => dispatch => {
+  _fetchUser(id, dispatch);
+};
+
+const _fetchUser = _.memoize(async (id, dispatch) => {
   const response = await API.get(`/users/${id}`);
 
   dispatch({ type: "FETCH_USER", payload: response.data });
-};
+});
--- src/components/PostList.jsx
+++ src/components/PostList.jsx
@@ -1,5 +1,4 @@
 import React, { Component } from "react";
-import API from "../utils/API";
 import { connect } from "react-redux";
 import { fetchPosts } from "../actions";
 import UserHeader from "./UserHeader";
